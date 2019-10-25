0eb7d56fe0d66da23f82d36b925f61bf6472367f
daeun <wjdekdms99@naver.com>
2019-10-25 17:57:03 +0900

    sort algorithm

---
+++ 19102505.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<vector>
+#include <algorithm>
+using namespace std;
+int solution(vector<int> A, vector<int> B){
+	sort(A.begin(), A.end());
+	reverse(A.begin(), A.end());
+	sort(B.begin(), B.end());
+	int answer = 0;
+	for (int i = 0; i < A.size(); i++)
+		answer += A[i] * B[i];
+	return answer;
+}
