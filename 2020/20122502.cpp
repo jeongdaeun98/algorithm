4e2c43b30448fffc0d341b8863fe366cd904ccb7
jeongdaeun <daeun@squarelab.co>
2020-12-26 23:09:30 +0900

    samsungSW and simulation

---
+++ 2020/20122502.cpp
@@ -0,0 +1,12 @@
+//드래곤 커브
+#include <iostream>
+using namespace std;
+
+struct point{
+    int x,y;
+};
+pair<int,int> direct[]={{0,1},{-1,0},{0,-1},{1,0}};
+pair<int, int>
+int main(void){
+    
+}
