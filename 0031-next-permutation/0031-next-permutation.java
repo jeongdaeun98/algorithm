500eb99daa7c2b1dbceef8f7beb9029dfd795a56
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2023-09-03 00:24:33 +0900

    Time: 1 ms (25.95%), Space: 42.2 MB (55.09%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int lastIndex = nums.length - 1;
+        boolean isNext = false;
+        for(int i = lastIndex; i > 0; i--) {
+            if(nums[i - 1] < nums[i]) {
+                isNext = true;
+                for(int j = lastIndex; j >= i; j--) {
+                    if(nums[i - 1] < nums[j]) {
+                        swap(i-1, nums, j);
+                        break;
+                    }
+                }
+                int[] copiedArray = Arrays.copyOfRange(nums, i, lastIndex + 1);
+                Arrays.sort(copiedArray);
+                replace(i, copiedArray, nums);
+                break;
+            }
+        }
+        if(!isNext) {
+            Arrays.sort(nums);
+        }
+    }
+    public void swap(int index, int[] nums, int changeIndex) {
+        int temp = nums[changeIndex];
+        nums[changeIndex] = nums[index];
+        nums[index] = temp;
+    }
+    public void replace(int index, int[] copiedArray, int[]nums) {
+        for(int i = 0; i < copiedArray.length; i++) {
+            nums[index + i] = copiedArray[i];
+        }
+    }
+}
