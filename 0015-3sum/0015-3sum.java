f3c3ae2c9ea3e739ddf726b25dfdf275b7d5338a
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2023-09-07 11:04:51 +0900

    Time: 31 ms (78.45%), Space: 50.1 MB (93.98%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -4,30 +4,30 @@ class Solution {
         List<List<Integer>> answer = new ArrayList<>();
         Arrays.sort(nums);
         for(int i = 0; i < nums.length; i++) {
-            if(i - 1 >= 0 && nums[i] == nums[i - 1]){
-                continue;
-            }
-            int left = i + 1;
-            int right = nums.length - 1;
-            while(left < right) {
-                int sum = nums[left] + nums[right] + nums[i];
-                if(sum == 0) {
-                    answer.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    left++;
-                    right--;
-                    while(nums[left] == nums[left - 1] && left < right) {
-                        left++;
-                    }
-                    while(nums[right] == nums[right + 1] && left < right) {
-                        right--;
-                    }
-                } else if(sum > 0) {
-                    right--;
-                } else {
-                    left++;
-                }
-            }
-        }
+           if(i - 1 >= 0 && nums[i] == nums[i - 1]) {
+               continue;
+           }
+           int left = i + 1;
+           int right = nums.length - 1;
+           while(left < right) {
+               int sum = nums[i] + nums[left] + nums[right];
+               if(sum == 0) {
+                   answer.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                   left++;
+                   right--;
+                   while(nums[left] == nums[left - 1] && left < right) {
+                       left++;
+                   }
+                   while(nums[right] == nums[right + 1] && left < right) {
+                       right--;
+                   }
+               } else if(sum > 0) {
+                   right--;
+               } else{
+                   left++;
+               }
+           }
+       }
         return answer;
     }
 }
