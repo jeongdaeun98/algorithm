a78e11623743e3f7afb62c9b9188374b57b19ce4
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2023-09-07 10:53:14 +0900

    Time: 30 ms (83.94%), Space: 50.6 MB (73.02%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> answer = new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length; i++) {
+            if(i - 1 >= 0 && nums[i] == nums[i - 1]){
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.length - 1;
+            while(left < right) {
+                int sum = nums[left] + nums[right] + nums[i];
+                if(sum == 0) {
+                    answer.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                    while(nums[left] == nums[left - 1] && left < right) {
+                        left++;
+                    }
+                    while(nums[right] == nums[right + 1] && left < right) {
+                        right--;
+                    }
+                } else if(sum > 0) {
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+        }
+        return answer;
+    }
+}
