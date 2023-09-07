653d486664b3be7fe38709819fd3efbc577c7789
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2023-09-07 21:49:18 +0900

    Time: 1 ms (100.00%), Space: 55.9 MB (12.00%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int sum = 0;
+        int total = 0;
+        int answer = 0;
+        for(int i = 0; i < gas.length; i++) {
+            sum += gas[i] - cost[i];
+            if(sum < 0) {
+                total += sum;
+                sum = 0;
+                answer = i + 1;
+            }
+        }
+        if(total + sum >= 0) {
+            return answer;
+        } else {
+            return -1;
+        }
+    }
+}
