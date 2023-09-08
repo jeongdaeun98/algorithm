b03383f96b14a40fa3cd9eb265c0275af68dcf53
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2023-09-08 15:18:51 +0900

    Time: 3 ms (44.08%), Space: 43 MB (88.40%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -17,20 +17,20 @@ class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         return generateTree(preorder, inorder,0, 0, inorder.length - 1);
     }
-    public TreeNode generateTree(int[]preorder, int[] inorder, int preorderIndex, int inorderStartIndex, int inorderEndIndex) {
-        if(preorderIndex >= preorder.length || inorderStartIndex > inorderEndIndex) {
+    public TreeNode generateTree(int[] preorder, int[] inorder,int preorderIndex,int inorderStartIndex, int inorderEndIndex) {
+        if(preorderIndex >= preorder.length || inorderStartIndex >inorderEndIndex) {
             return null;
         }
         TreeNode node = new TreeNode(preorder[preorderIndex]);
         int inorderIndex = 0;
         for(int i = inorderStartIndex; i <= inorderEndIndex; i++) {
-            if(preorder[preorderIndex] == inorder[i]){
+            if(node.val == inorder[i]) {
                 inorderIndex = i;
                 break;
             }
         }
-        node.left = generateTree(preorder, inorder, preorderIndex + 1, inorderStartIndex, inorderIndex - 1);
-        node.right = generateTree(preorder, inorder, preorderIndex + inorderIndex - inorderStartIndex + 1, inorderIndex + 1, inorderEndIndex);
+        node.left = generateTree(preorder, inorder, preorderIndex + 1, inorderStartIndex, inorderIndex -1);
+        node.right = generateTree(preorder, inorder, preorderIndex + inorderIndex - inorderStartIndex +1, inorderIndex + 1, inorderEndIndex);
         return node;
     }
 }
