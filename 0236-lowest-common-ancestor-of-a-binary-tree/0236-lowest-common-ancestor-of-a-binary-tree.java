9ec31ef1a2006b3091ec7d69713ecac52ed187a3
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2023-09-08 23:41:34 +0900

    Time: 6 ms (97.96%), Space: 43.7 MB (46.96%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -11,23 +11,22 @@ import java.util.*;
 class Solution {
     TreeNode answer = null;
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        Set<Integer> nodes = new HashSet<>();
-        traversal(root, p, q);
+        traversal(root,p,q);
         return answer;
     }
     public int traversal(TreeNode node, TreeNode p, TreeNode q) {
         int count = 0;
         if(node != null) {
-            if(node.val == p.val || node.val == q.val) {
-                count++;
+            if(node.val == p.val || node.val == q.val){
+                count ++;
             }
-            count += traversal(node.left, p, q);
+            count += traversal(node.left, p , q);
             count += traversal(node.right, p, q);
             if(count == 2 && answer == null) {
                 answer = node;
             }
-            return count;
         }
-        return 0;
+        return count;
     }
+        
 }
