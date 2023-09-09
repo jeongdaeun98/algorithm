1712ce5dedbd106ff77d4c46de9bfadb19989352
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2023-09-09 19:32:27 +0900

    Time: 151 ms (61.40%), Space: 40.6 MB (63.01%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,39 +1,36 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        boolean visited[][] = new boolean[board.length][board[0].length];
-        if(word.length() == 0) {
-            return true;
-        }
-        for(int i = 0; i < board.length ; i++) {
+        boolean[][] visited = new boolean[board.length][board[0].length];
+        for(int i = 0; i < board.length; i++) {
             for(int j = 0; j < board[i].length; j++) {
-                if(combination(board, word,0, i,j, visited)) {
+                if(combination(board, visited, word, 0, i, j)){
                     return true;
                 }
             }
         }
         return false;
     }
-    public boolean combination(char[][]board, String word, int index, int i, int j, boolean visited[][]) {
-        if(word.length() == index) {
+    
+    public boolean combination(char[][] board, boolean[][] visited, String word, int index, int i, int j) {
+        if(index == word.length()) {
             return true;
         }
-        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length){
+        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length) {
             return false;
         }
-        if(visited[i][j]){
+        if(visited[i][j]) {
             return false;
         }
-        if(word.charAt(index) == board[i][j]) {
+        if(word.charAt(index) == board[i][j]){
             visited[i][j] = true;
-            boolean result = combination(board, word, index + 1, i, j + 1, visited) ||
-                combination(board, word, index + 1, i + 1, j, visited) ||
-                combination(board, word, index+ 1, i, j -1, visited) ||
-                combination(board, word, index + 1, i - 1, j, visited);
-            if(result){
-                return true;
-            } else {
+            boolean result = combination(board, visited, word, index + 1, i + 1, j)
+            || combination(board, visited, word, index + 1, i, j + 1)
+            || combination(board, visited, word, index + 1, i - 1, j)
+            || combination(board, visited, word, index + 1, i, j - 1);
+            if(!result) {
                 visited[i][j] = false;
             }
+            return result;
         }
         return false;
     }
