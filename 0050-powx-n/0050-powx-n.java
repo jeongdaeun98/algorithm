ce294444e97b183674c57865fa5f6f13e8b7aa0a
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2023-09-09 14:08:12 +0900

    Time: 0 ms (100.00%), Space: 40.5 MB (77.30%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,24 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n == 0) {
+            return 1;
+        }
+        if(n < 0) {
+            if((n * -1) % 2 == 0) {
+               double temp = myPow(x, n / 2 * -1);
+                return 1 / (temp * temp);
+            } else {
+                double temp = myPow(x, n / 2 * -1);
+                return 1 / (temp * temp * x);
+            }
+        } else {
+            if(n % 2 == 0) {
+                double temp = myPow(x, n / 2);
+                return temp * temp;
+            } else {
+                double temp = myPow(x, n / 2);
+                return temp * temp * x;
+            }
+        }
+    }
+}
