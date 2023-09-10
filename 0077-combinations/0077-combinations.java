bce859e70b90cf33baa1ddd3c1a39d06d9c71e9a
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2023-09-10 15:27:11 +0900

    Time: 26 ms (15.70%), Space: 92 MB (20.69%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,25 +1,21 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> answers = new ArrayList<>();
-        List<Integer> numbers = new ArrayList<>();
-        for(int i = 0; i < n; i++) {
-            numbers.add(i + 1);
-        }
-        int[] answer = new int[k];
-        combination(k - 1, answers, answer, 0, numbers);
+        LinkedList<Integer> answer = new LinkedList<>();
+        combination(answers, answer, 1, k, n);
         return answers;
     }
-    
-    public void combination(int depth, List<List<Integer>> answers, int[] answer,int index, List<Integer> numbers) {
-        if(depth < 0) {
-            answers.add(Arrays.stream(answer).boxed().collect(Collectors.toList()));
+    public void combination(List<List<Integer>> answers, LinkedList<Integer> answer, int index, int k, int n) {
+        if(answer.size() == k) {
+            answers.add(new ArrayList<>(answer));
             return;
         }
-        if(index >= numbers.size()) {
+        if(index > n) {
             return;
         }
-        combination(depth, answers, answer, index + 1, numbers);
-        answer[depth] = numbers.get(index);
-        combination(depth -1, answers, answer, index + 1, numbers);
+        combination(answers, answer, index + 1, k, n);
+        answer.add(index);
+        combination(answers, answer, index + 1, k, n);
+        answer.removeLast();
     }
 }
