107362102302f2bf0cc5d5d3465696441878044b
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2023-09-10 15:59:30 +0900

    Time: 5 ms (48.73%), Space: 41.7 MB (6.06%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,36 +1,38 @@
-import java.util.*;
 class Solution {
     public List<String> letterCombinations(String digits) {
-        List<String> answers = new ArrayList<>();
-        Map<String, List<String>> digitAndAlphabet = new HashMap<>();
-        digitAndAlphabet.put("2", Arrays.asList("a","b", "c"));
-        digitAndAlphabet.put("3", Arrays.asList("d","e", "f"));
-        digitAndAlphabet.put("4", Arrays.asList("g","h", "i"));
-        digitAndAlphabet.put("5", Arrays.asList("j","k", "l"));
-        digitAndAlphabet.put("6", Arrays.asList("m","n", "o"));
-        digitAndAlphabet.put("7", Arrays.asList("p","q", "r","s"));
-        digitAndAlphabet.put("8", Arrays.asList("t","u", "v"));
-        digitAndAlphabet.put("9", Arrays.asList("w","x", "y", "z"));
-        List<List<String>> alphabet = new ArrayList<>();
-        for(int i = 0; i < digits.length(); i++){
-            String str = Character.toString(digits.charAt(i));
-            if(digitAndAlphabet.containsKey(str)){
-                alphabet.add(digitAndAlphabet.get(str));
+        Map<Character, String> numberAndLetters = new HashMap<>();
+        List<String> answer = new ArrayList<>();
+        if(digits.equals("")) {
+            return answer;
+        }
+        numberAndLetters.put('2', "abc");
+        numberAndLetters.put('3', "def");
+        numberAndLetters.put('4', "ghi");
+        numberAndLetters.put('5', "jkl");
+        numberAndLetters.put('6', "mno");
+        numberAndLetters.put('7', "pqrs");
+        numberAndLetters.put('8', "tuv");
+        numberAndLetters.put('9', "wxyz");
+        List<String> letters = new ArrayList<>();
+        for(int i = 0; i < digits.length(); i++) {
+            if(numberAndLetters.containsKey(digits.charAt(i))) {
+                letters.add(numberAndLetters.get(digits.charAt(i)));
             }
         }
-        combination("", answers, alphabet, 0, 0);
-        return answers;
+        combination(letters, answer, 0, "");
+        return answer;
     }
-    public void combination(String answer, List<String> answers, List<List<String>> alphabet, int index, int target) {
-        if(target == alphabet.size() && !answer.equals("")) {
-            answers.add(answer);
+    public void combination(List<String> letters, List<String> answer, int index, String letter){
+        if(letter.length() == letters.size()) {
+            answer.add(letter);
             return;
         }
-        if(target == alphabet.size() || alphabet.get(target).size() <= index) {
+        if(index >= letters.size()) {
             return;
         }
-        for(int i = index; i < alphabet.get(target).size(); i++){
-            combination(answer + alphabet.get(target).get(i), answers, alphabet, index, target + 1);
+        for(int i = 0; i < letters.get(index).length(); i++) {
+            combination(letters, answer, index + 1, letter + letters.get(index).charAt(i));
+            combination(letters, answer, index + 1, letter);
         }
     }
 }
