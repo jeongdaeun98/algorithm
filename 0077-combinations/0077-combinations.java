cc0bef06812f41b15d942d580239226ab7358e9d
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2023-09-10 15:32:13 +0900

    Time: 30 ms (12.06%), Space: 90.6 MB (24.72%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -2,10 +2,10 @@ class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> answers = new ArrayList<>();
         LinkedList<Integer> answer = new LinkedList<>();
-        combination(answers, answer, 1, k, n);
+        combination(answer, answers, n, k, 1);
         return answers;
     }
-    public void combination(List<List<Integer>> answers, LinkedList<Integer> answer, int index, int k, int n) {
+    public void combination(LinkedList<Integer> answer, List<List<Integer>> answers, int n, int k, int index) {
         if(answer.size() == k) {
             answers.add(new ArrayList<>(answer));
             return;
@@ -13,9 +13,9 @@ class Solution {
         if(index > n) {
             return;
         }
-        combination(answers, answer, index + 1, k, n);
         answer.add(index);
-        combination(answers, answer, index + 1, k, n);
+        combination(answer, answers, n, k, index + 1);
         answer.removeLast();
+        combination(answer, answers, n, k, index + 1);
     }
 }
