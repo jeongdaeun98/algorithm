d93ff9d21cd971b47b53c1d6a058ba2334f14772
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2023-09-30 21:03:09 +0900

    Time: 5 ms (5.45%), Space: 56.9 MB (5.59%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,21 +1,28 @@
 import java.util.*;
+// 0 1 2 3 4 5 6
+//[1,2,3,4,5,6,7]
+// k = 3
+// 3 4 5 6 0 1 2
+//[5,6,7,1,2,3,4]
+class NumIndex {
+    int number;
+    int index;
+    public NumIndex(int number, int index) {
+        this.number = number;
+        this.index = index;
+    }
+}
 class Solution {
     public void rotate(int[] nums, int k) {
-        List<List<Integer>> numberIndexes = new ArrayList<>();
-        int length = nums.length;
-        for (int i = 0; i < length; i++)  {
-            numberIndexes.add(new ArrayList<>());
-        }
-
+        int index;
+        List<NumIndex> numbers = new ArrayList<>();
         for(int i = 0; i < nums.length; i++) {
-            List<Integer> indexes = numberIndexes.get(i);
-            indexes.add(nums[i]);
-            int num = i + k;
-            indexes.add(num % length);
+            index = (k + i) % nums.length;
+            numbers.add(new NumIndex(nums[i], index));
         }
-        for(int i = 0; i < nums.length; i++) {
-            List<Integer> indexes = numberIndexes.get(i);
-            nums[indexes.get(1)] = indexes.get(0); 
+        for(int i = 0; i < numbers.size(); i++) {
+            NumIndex numIndex = numbers.get(i);
+            nums[numIndex.index] = numIndex.number;
         }
     }
 }
