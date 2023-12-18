b610b3a5230fb9fa17472a9115665d20b9b8009e
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2023-12-18 10:34:09 +0900

    Time: 1 ms (23.31%), Space: 42.5 MB (12.71%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,36 +1,34 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int index = 0;
+        int lastIndex = nums.length - 1;
         boolean isNext = false;
-        for(int i = nums.length - 1; i > 0; i--) {
+        for(int i = lastIndex; i > 0; i--) {
             if(nums[i - 1] < nums[i]) {
-                index = i - 1;
                 isNext = true;
+                for(int j = lastIndex; j >= i; j--) {
+                    if(nums[i - 1] < nums[j]) {
+                        swap(i - 1,nums, j);
+                        break;
+                    }
+                }
+                int[] copiedArray = Arrays.copyOfRange(nums, i, lastIndex + 1);
+                Arrays.sort(copiedArray);
+                replace(i, copiedArray, nums);
                 break;
             }
         }
         if(!isNext) {
             Arrays.sort(nums);
-            return;
         }
-        for(int i = nums.length - 1; i > index; i--) {
-            if(nums[i] > nums[index]) {
-                swap(i, index, nums);
-                break;
-            }
-        }
-        int[] copy = Arrays.copyOfRange(nums, index + 1, nums.length);
-        Arrays.sort(copy);
-        replace(nums, copy, index + 1);
     }
-    public void swap(int i, int j, int[] nums) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+    public void swap(int index, int[] nums, int changeIndex) {
+        int temp = nums[changeIndex];
+        nums[changeIndex] = nums[index];
+        nums[index] = temp;
     }
-    public void replace(int[] nums, int[] copy, int index) {
-        for(int i = 0; i < copy.length; i++) {
-            nums[index + i] = copy[i];
+    public void replace(int index, int[] copiedArray, int[]nums) {
+        for(int i = 0; i < copiedArray.length; i++) {
+            nums[index + i] = copiedArray[i];
         }
     }
 }
