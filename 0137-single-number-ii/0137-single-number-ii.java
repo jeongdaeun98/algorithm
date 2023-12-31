36fb46df8b21958fcaa66048790d2ea82f6526d1
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2023-12-31 20:43:10 +0900

    Time: 16 ms (7.50%), Space: 45.1 MB (15.70%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,16 +1,24 @@
 import java.math.BigInteger;
 class Solution {
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        for(int i = 0; i < nums.length - 2; i = i + 3) {
-            BigInteger a = new BigInteger(Integer.toBinaryString(nums[i]), 2);
-            BigInteger b = new BigInteger(Integer.toBinaryString(nums[i + 1]), 2);
-            BigInteger c = new BigInteger(Integer.toBinaryString(nums[i + 2]), 2);
-            BigInteger zero = new BigInteger("0");
-            if(a.xor(b).compareTo(zero) != 0 && b.xor(c).compareTo(zero) == 0) {
-                return a.intValue();
+        String sum = "";
+        int[] digit = new int[32];
+        Arrays.fill(digit, 0);
+        for(int i = 0; i < nums.length; i++) {
+            String number = Integer.toBinaryString(nums[i]);
+            for(int j = 0; j < number.length(); j++) {
+                if(number.charAt(j) == '1'){
+                    digit[number.length() - 1 - j]++;
+                }
             }
         }
-        return nums[nums.length - 1];
+        for(int i = digit.length - 1; i >= 0; i--) {
+            if(digit[i] % 3 > 0){
+                sum += "1";
+            } else {
+                sum += "0";
+            }
+        }
+        return new BigInteger(sum, 2).intValue();
     }
 }
