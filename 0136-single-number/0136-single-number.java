48f62a9f17c855964d683d3c9aab6b66f710e08e
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2023-12-31 20:06:39 +0900

    Time: 1 ms (99.92%), Space: 45.7 MB (14.34%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,13 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        for(int i = 0; i < nums.length - 1; i++) {
-            if(nums[i] != nums[i + 1]){
-                return nums[i];
-            } else {
-                i++;
-            }
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++) {
+            sum ^= nums[i];
         }
-        return nums[nums.length - 1];
+        return sum;
     }
 }
