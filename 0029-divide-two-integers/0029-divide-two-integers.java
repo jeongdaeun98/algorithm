a9bc84458597c3c6c239be875b884c507761d487
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2023-12-31 17:29:24 +0900

    Time: 10 ms (6.97%), Space: 42.7 MB (5.29%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -1,15 +1,16 @@
 import java.math.*;
+
 class Solution {
     public int divide(int dividend, int divisor) {
         BigInteger dividendBig = new BigInteger(Integer.toString(dividend));
         BigInteger divisorBig = new BigInteger(Integer.toString(divisor));
-        BigInteger bigNum = dividendBig.divide(divisorBig);
-        if(bigNum.compareTo(new BigInteger(Integer.toString(Integer.MAX_VALUE))) == 1) {
+        BigInteger result = dividendBig.divide(divisorBig);
+        if(result.compareTo(new BigInteger(Integer.toString(Integer.MAX_VALUE))) > 0) {
             return Integer.MAX_VALUE;
         }
-        if(bigNum.compareTo(new BigInteger(Integer.toString(Integer.MIN_VALUE))) == -1) {
+        if(result.compareTo(new BigInteger(Integer.toString(Integer.MIN_VALUE))) < 0) {
             return Integer.MIN_VALUE;
         }
-        return bigNum.intValue();
+        return result.intValue();
     }
 }
