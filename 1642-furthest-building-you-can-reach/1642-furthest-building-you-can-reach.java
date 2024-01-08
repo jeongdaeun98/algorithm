9f23ceeb06941e9abcd238e4a29e4daa6e1abe4b
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-01-08 13:40:46 +0900

    Time: 20 ms (26.61%), Space: 57.3 MB (50.17%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
@@ -2,17 +2,18 @@ import java.util.*;
 class Solution {
     public int furthestBuilding(int[] heights, int bricks, int ladders) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int answer = 0;
         for(int i = 0; i < heights.length - 1; i++) {
-            int diff = heights[i + 1] - heights[i];
-            if(diff > 0) {
-                pq.add(diff);
+            if(heights[i] < heights[i + 1]) {
+                pq.offer(heights[i + 1] - heights[i]);
             }
             if(pq.size() > ladders) {
-                bricks -= pq.poll();
-                if(bricks < 0) {
-                    return i;
-                }
+                bricks -=  pq.poll();
             }
+            if(bricks < 0) {
+                return answer;
+            }
+            answer++;
         }
         return heights.length - 1;
     }
