9dfe29a2d728546531a09ee85dc8f1be52a7afd7
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-01-11 20:50:33 +0900

    Time: 2 ms (37.79%), Space: 43 MB (65.57%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,19 +1,22 @@
+// 1. permutation으로 구현, left와 right를 n으로 요청
+// 2. right는 left보다 클 때만 추가되도록 함.
 class Solution {
     public List<String> generateParenthesis(int n) {
-        List<String> parenthesis = new ArrayList<>();
-        generate(parenthesis, n, n, "");
-        return parenthesis;
+        List<String> answers = new ArrayList<>();
+        permutation(n, n, answers, "");
+        return answers;
     }
-    public void generate(List<String> parenthesis, int left, int right, String parenthese) {
+    public void permutation(int left, int right, List<String> answers, String answer) {
         if(left == 0 && right == 0) {
-            parenthesis.add(parenthese);
+            answers.add(answer);
+            return;
+        }
+        if((left < 0 || right < 0) ||(left > right)) {
             return;
         }
         if(left > 0) {
-            generate(parenthesis, left - 1, right, parenthese + "(");
+            permutation(left - 1, right, answers, answer + "(");
         } 
-        if(left < right) {
-            generate(parenthesis, left, right - 1, parenthese + ")");
-        }
+        permutation(left, right - 1, answers, answer + ")");
     }
 }
