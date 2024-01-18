cdc454b2d43dce16976c0a11e8a09e9a309956cc
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-01-18 16:40:13 +0900

    Time: 8 ms (40.55%), Space: 44.4 MB (22.09%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,20 +1,19 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        HashSet<String> hs = new HashSet<>();
-        for(String str : wordDict) hs.add(str);
-        
-        boolean[] dp = new boolean[s.length() + 1];
+        Set<String> dicts = new HashSet<>();
+        for(String word : wordDict) {
+            dicts.add(word);
+        }
+        boolean dp[] = new boolean[s.length() + 1];
         dp[0] = true;
-        
-        for(int i = 1; i <= s.length(); i++){
-            for(int j = i - 1; j >= 0; j--){
-                if(dp[j] && hs.contains(s.substring(j, i))){
+        for(int i = 1; i <= s.length(); i++) {
+            for(int j = 0; j < i; j++) {
+                if(dp[j] && dicts.contains(s.substring(j, i))) {
                     dp[i] = true;
                     break;
                 }
             }
         }
-        
         return dp[s.length()];
     }
 }
