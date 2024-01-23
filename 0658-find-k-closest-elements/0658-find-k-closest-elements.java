a13400dd2188d784b24fc3bcc9336160660473ab
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-01-23 21:50:47 +0900

    Time: 5 ms (57.18%), Space: 45.9 MB (27.37%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -2,8 +2,8 @@
 // 2. x를 기준 값으로 x - arr[mid] 와 arr[mid + k] - x 차를 확인했을 때, 
 // 3. x - arr[mid]가 더 크다면 오른쪽으로
 // 4. arr[mid + k] - x가 더 크다면 왼쪽으로 슬라이딩 해야함.
-// 5. 오른쪽 슬라이딩은 left = mid + 1로 하면 x - arr[mid + 1] arr[mid + 1 - k] - x이므로 오른쪽 슬라이딩
-// 6. 왼쪽 슬라이딩은 right = mid 로 한다면 x - arr[]
+// 5. 오른쪽 슬라이딩은 mid mid - 1 mid - 2 ...는 볼필요 없는 것이므로 left = mid + 1
+// 6. 왼쪽 슬라이딩은 k는 1이상이므로, mid + 1 mid + 2 mid + 3..는 볼필요 없으니 right = mid
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         int left = 0, right = arr.length - k;
