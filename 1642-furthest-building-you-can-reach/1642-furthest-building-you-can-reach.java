63485887cffa92bdf1d39360bab01a4d637cb988
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-02-05 18:14:12 +0900

    Time: 21 ms (18.78%), Space: 57 MB (79.73%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
@@ -1,3 +1,7 @@
+// 내려가는 케이스일 경우,패스& answer ++
+// 올라가는 케이스일 경우, 올라가는 벽돌 개수를 pq에 넣음
+// pq의 사이즈가 사다리 보다 넘을 때, pq 작은 순으로 빼서 bricks 제거
+// bricks가 0 미만일 경우, answer 리턴
 import java.util.*;
 class Solution {
     public int furthestBuilding(int[] heights, int bricks, int ladders) {
@@ -8,13 +12,13 @@ class Solution {
                 pq.offer(heights[i + 1] - heights[i]);
             }
             if(pq.size() > ladders) {
-                bricks -=  pq.poll();
+                bricks -= pq.poll();
             }
             if(bricks < 0) {
                 return answer;
             }
             answer++;
         }
-        return heights.length - 1;
+        return answer;
     }
 }
