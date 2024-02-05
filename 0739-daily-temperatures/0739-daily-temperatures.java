fcb6c176deda2ef5e7400aa27adeb547faf4f019
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-02-05 20:07:06 +0900

    Time: 65 ms (50.90%), Space: 63 MB (13.45%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,16 +1,17 @@
-//1. stack에 값을 넣을 때, 인덱스를 넣음
-//2. 맨 앞 값보다 작은 값이라면 STack에 넣음
-//3. 저장된 인덱스보다 큰 값이라면 기존 값 빼고, 현 인덱스 - 기존 값 을 기존 값에 넣고, 다시 넣음.
-//4. 만들어진 array 리턴
+// 인덱스대로 스택에 저장
+// 해당 인덱스가 가리키는 값과 순회하는 값 비교 후, 순회 값이 더 크면 인덱스 빼냄
+// 그 인덱스에 순회 값 - 인덱스 가리키는 값
+// int 배열 리턴
+import java.util.*;
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         int[] answer = new int[temperatures.length];
+        Arrays.fill(answer, 0);
         Stack<Integer> stack = new Stack<>();
-        stack.push(0);
-        for(int i = 1; i < temperatures.length; i++){
-            while(!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
-                answer[stack.peek()] = i - stack.peek();
-                stack.pop();
+        for(int i = 0; i < temperatures.length; i++) {
+            while(!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]){
+                int index = stack.pop();
+                answer[index] = i - index;
             }
             stack.push(i);
         }
