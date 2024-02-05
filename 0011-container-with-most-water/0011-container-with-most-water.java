435a4017836544d1529fe0415605e0a9f4b25674
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-02-05 18:05:18 +0900

    Time: 5 ms (68.49%), Space: 58.1 MB (30.25%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,13 +1,16 @@
+// left right 포인터 설정
+// left와 right 중 값이 작은 애 인덱스를 증가
+// Math.min(height[left], height[right]) * (right - left)
+// left가 right 넘을 때까지 반복
 class Solution {
     public int maxArea(int[] height) {
-        int left = 0, right = height.length - 1;
         int answer = 0;
+        int left = 0, right = height.length - 1;
         while(left < right) {
+            answer = Math.max(answer, Math.min(height[left], height[right]) * (right - left));
             if(height[left] < height[right]) {
-                answer = Math.max((right - left)*height[left], answer);
                 left++;
             } else {
-                answer = Math.max((right - left)*height[right], answer);
                 right--;
             }
         }
