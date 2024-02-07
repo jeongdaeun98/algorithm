b36a4f51ed78c8d8d33760d231f7afb7faf5c576
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-02-08 01:10:09 +0900

    Time: 0 ms (100.00%), Space: 41.6 MB (60.33%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,21 +1,20 @@
-// 1. buy sell rest로 저장소를 나누어서 가장 큰 값으로 갱신한다.
-// 2. buy[i] = Math.max(rest[i - 1] - prices, buy[i - 1]);
-// 3. sell[i] = Math.max(buy[i - 1] + prices, sell[i - 1]);
-// 4. rest[i] = Math.max(buy[i - 1], sell[i - 1], rest[i - 1]);
-// 5. rest의 경우 buy[i - 1] <= rest[i - 1] <= sell[i - 1]
-// 6. rest[i] = sell[i - 1]이다.
-// 7. buy[i] = Math.max(sell[i - 2] - prices, buy[i - 1]);
-// 8. sell[i] = Math.max(buy[i - 1] + prices, sell[i - 1]);
+// [1,2,3,0,2]
+// buy[i] = Math.max(rest[i - 1] - prices, buy[i - 1])
+// sell[i] = Math.max(buy[i - 1] + prices[i], sell[i - 1])
+// rest[i] = Math.max(sell[i - 1], buy[i - 1], rest[i - 1])
+// buy[i] + sell[i] + rest[i]
+// buy = Math.max(sell[i - 2] - prices[i] , buy[i - 1])
+// sell = Math.max(buy[i - 1] + prices[i], sell[i - 1])
+// Math.max(buy[i - 2] - prices[i] , buy[i - 1]) + Math.max(buy[i - 1] + prices[i], sell[i - 1]) + buy[i - 1]
 
 class Solution {
     public int maxProfit(int[] prices) {
-        int sell = 0, prevSell = 0;
-        int buy = Integer.MIN_VALUE, prevBuy = Integer.MIN_VALUE;
-        for(int i = 0; i < prices.length; i++) {
+        int prevBuy = Integer.MIN_VALUE, buy = Integer.MIN_VALUE, sell = 0, prevSell = 0;
+        for(int i = 0; i < prices.length; i++){
             prevBuy = buy;
-            buy = Math.max(prevSell - prices[i], prevBuy);
+            buy = Math.max(prevSell - prices[i], buy);
             prevSell = sell;
-            sell = Math.max(prevBuy + prices[i], prevSell);
+            sell = Math.max(prevBuy + prices[i], sell);
         }
         return sell;
     }
