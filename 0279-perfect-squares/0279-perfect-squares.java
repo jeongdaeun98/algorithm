9822bda816393a33c5270ae50e7de89c00e05948
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-02-07 16:15:50 +0900

    Time: 32 ms (71.71%), Space: 42.5 MB (79.96%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,12 +1,13 @@
-// 1. dp 로 n만큼 순회
-// 2. dp[n - 순회 인덱스] + dp [인덱스]와 dp[n] 중 가장 작은 값으로 뒤바꿈
+// 1부터 n까지 순회
+// 1*1 2*2 3*3 ... n*n  = 1
+// 순회하면서 dp[n - j * j] + 1
 class Solution {
     public int numSquares(int n) {
-        int dp[] = new int[n + 1];
+        int[] dp = new int[n + 1];
         Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
         for(int i = 1; i <= n; i++) {
-            for(int j = 1; j * j <= i; j++){
+            for(int j = 1; j * j <= i; j++) {
                 dp[i] = Math.min(dp[i - j * j] + 1, dp[i]);
             }
         }
