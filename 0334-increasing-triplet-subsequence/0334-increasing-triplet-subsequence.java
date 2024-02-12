0e6729a61c940cfd580f38702e1bdddce567b740
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-02-12 15:49:39 +0900

    Time: 2 ms (94.01%), Space: 133.8 MB (48.37%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -1,20 +1,26 @@
-// 1. 순회
-// 2. i 가장 작은 값
-// 3. 순회할 때마다 가장 작은 값보다 더 작은지 확인, 더 작으면 i 갱신 후 j와 k를 null로 갱신
-// 4. 더 작지는 않으면, j보다 더 작은지 확인, 그러면 j를 갱신
-// 5. i보다 작지는 않고, j보다 작으면 j를 갱신
-// 6. 마지막에서 i,j,k가 null이 아니라면 true 리턴
+// 순회
+// 가장 작은 값 i로 갱신
+// i보다 큰 값 j 갱신
+// j보다 크고 i보다 큰 값 k로 갱신
+import java.util.*;
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-        int i = Integer.MAX_VALUE, j = Integer.MAX_VALUE;
+        int i = Integer.MAX_VALUE, j = i, k = i;
+        boolean ip = false, jp = false, kp = false;
         for(int index = 0; index < nums.length; index++) {
             if(i >= nums[index]) {
                 i = nums[index];
+                ip = true;
             } else if(j >= nums[index]) {
                 j = nums[index];
-            } else {
-                return true;
+                jp = true;
+            } else if(k >= nums[index]) {
+                k = nums[index];
+                kp = true;
             }
+        }
+        if(ip && jp && kp) {
+            return true;
         }
         return false;
     }
