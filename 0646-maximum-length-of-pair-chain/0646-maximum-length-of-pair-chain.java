0a42f69c2d077a05a55f8e919f4658ad7a01eee6
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-02-13 10:40:33 +0900

    Time: 8 ms (99.37%), Space: 45.3 MB (25.24%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
@@ -1,29 +1,17 @@
-//왼쪽 수대로 정렬
-// 이중 포문으로 맨 앞부터 뒤에까지 pair 개수 많은 거 리턴
+// 다음 인자의 left는 반드시 인자의 right보다 크기 때문에
+// 정렬
+// 순회
+// right를 마지막 range로 설정
+// 다음 인자의 left와 비교하여 left가 클 경우, answer증가하면서 마지막 range 를 right로 설정
 class Solution {
     public int findLongestChain(int[][] pairs) {
+        Arrays.sort(pairs, (a,b) -> a[1] - b[1]);
         int answer = 1;
-        Arrays.sort(pairs, (a1, a2) -> {
-            if(a1[0] == a2[0]) {
-                return a1[1] - a2[1];
-            }
-            return a1[0] - a2[0];
-        });
-        for(int i = 0; i < pairs.length; i++) {
-            int left = pairs[i][0];
-            int right = pairs[i][1];
-            int count = 1;
-            for(int j = i + 1; j < pairs.length; j++) {
-                if(right > pairs[j][1]) {
-                    right = pairs[j][1];
-                    left = pairs[j][0];
-                } else if(pairs[j][0] > right) {
-                    count++;
-                    left = pairs[j][0];
-                    right = pairs[j][1];
-                }
-                answer = Math.max(answer, count);
-                
+        int lastRange = pairs[0][1];
+        for(int i = 1; i < pairs.length; i++) {
+            if(lastRange < pairs[i][0]) {
+                answer++;
+                lastRange = pairs[i][1];
             }
         }
         return answer;
