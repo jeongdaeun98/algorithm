82ffb9d33f12c2984224b751cf5cd34b03228449
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-02-22 20:43:27 +0900

    Time: 0 ms (100.00%), Space: 41.7 MB (11.44%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,21 +1,19 @@
-// 왼쪽이 *로 대체되는 경우, 오른쪽이 *로 대체되는 경우를 나누어서 저장.
-// 왼쪽인 경우, 오른쪽이 0보다 크다면 false를
-// 오른쪽인 경우, 왼쪽이 0보다 작다면 false를
-// 그 외라면 true 리턴
+// 순회하면서는 언제나 left + wild >= right
+// 나와서는 left > right일 경우, left - right =< wild라면 패스
+// left < right 라면, right - left <= wild 라면 패스
 class Solution {
     public boolean checkValidString(String s) {
         int left = 0, right = 0;
         for(int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if(c == '(') {
-                left ++;
-                right ++;
-            } else if(c == ')') {
-                left --;
-                right --;
-            } else {
-                left ++;
-                right --;
+            if(s.charAt(i) == '(') {
+                left++;
+                right++;
+            }else if(s.charAt(i) == ')') {
+                right--;
+                left--;
+            }else {
+               left++;
+                right--;
             }
             if(left < 0) {
                 return false;
