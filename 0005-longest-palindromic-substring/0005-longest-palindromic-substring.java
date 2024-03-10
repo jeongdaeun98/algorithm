cd5c0e62cedc05efaa64db1a80539147f60986d5
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-03-10 14:44:16 +0900

    Time: 201 ms (26.48%), Space: 41.8 MB (95.58%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,22 +1,20 @@
+// string 길이부터 1까지 길이를 줄여나가면서 palindrome인지 확인하는 방법
 class Solution {
     public String longestPalindrome(String s) {
-        for (int len = s.length(); len > 1; len--) {
-            for (int idx = 0; idx + len <= s.length(); idx++) {
-                boolean isValid = true;
-
-                for (int i = 0; i < len / 2; i++) {
-                    if (s.charAt(idx + i) != s.charAt(idx + len - i - 1)) {
-                        isValid = false;
+        for(int i = s.length(); i > 1; i--) {
+            for(int j = 0; i + j <= s.length(); j++) {
+                boolean valid = true;
+                for(int index = 0; index < i / 2; index++) {
+                    if(s.charAt(index + j) != s.charAt(j + i - index - 1)) {
+                        valid = false;
                         break;
                     }
                 }
-
-                if (isValid) {
-                    return s.substring(idx, idx + len);
+                if(valid) {
+                    return s.substring(j, j + i);
                 }
             }
         }
-
-        return s.substring(0, 1);
+        return s.substring(0,1);
     }
 }
