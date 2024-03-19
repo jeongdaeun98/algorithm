063de2f3abe4fec814b72696bba4ab5fd84da231
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-03-19 23:30:15 +0900

    Time: 867 ms (13.33%), Space: 57.4 MB (37.17%) - LeetHub

--- 0848-shifting-letters/0848-shifting-letters.java
+++ 0848-shifting-letters/0848-shifting-letters.java
@@ -1,19 +1,22 @@
-// 누적해서 sum 구함
-// 1의 자릿 수부터 sum[shifts.length - 1] sum[shifts.length - 1] - sum[0].... % ('z' - 'a')
+// 3 5 9
+//17 14 9
+// a  b c
+// a 3 + 5 + 9 
+// b 5 + 9 
+// c 9
+// 뒤부터 누적합
+// z-a + 1을 배열크기로
+// 인덱스 - a % 배열크기
 class Solution {
     public String shiftingLetters(String s, int[] shifts) {
-        int[] sum = new int[shifts.length];
-        sum[0] = shifts[0];
-        for(int i = 1; i < shifts.length; i++) {
-            sum[i] = shifts[i] % 26 + sum[i - 1];
+        int[] sum = new int[shifts.length + 1];
+       
+        for(int i = shifts.length - 1; i >= 0; i--) {
+            sum[i] = (shifts[i] + sum[i + 1]) % ('z' - 'a' + 1);
         }
         String answer = "";
         for(int i = 0; i < s.length(); i++) {
-            if (i - 1 >= 0){
-            answer += Character.toString((s.charAt(i) - 'a' + sum[s.length() - 1] - sum[i - 1])%26 + 'a');
-            } else {
-                answer += Character.toString((s.charAt(i) - 'a' + sum[s.length() - 1]) %26 + 'a');
-            }
+            answer += Character.toString((s.charAt(i) - 'a' + sum[i]) % ('z' - 'a' + 1) + 'a');
         }
         return answer;
     }
