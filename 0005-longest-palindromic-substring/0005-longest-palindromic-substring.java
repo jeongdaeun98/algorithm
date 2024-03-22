40371fdd8c11ab89827503b30aeacfdcd1a5858b
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-03-22 11:01:51 +0900

    Time: 493 ms (20.20%), Space: 45 MB (49.47%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,36 +1,20 @@
-public class Solution {
+class Solution {
     public String longestPalindrome(String s) {
-        if (s.length() <= 1) {
-            return s;
-        }
-
-        int maxLen = 1;
-        String maxStr = s.substring(0, 1);
-        s = "#" + s.replaceAll("", "#") + "#";
-        int[] dp = new int[s.length()];
-        int center = 0;
-        int right = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            if (i < right) {
-                dp[i] = Math.min(right - i, dp[2 * center - i]);
-            }
-
-            while (i - dp[i] - 1 >= 0 && i + dp[i] + 1 < s.length() && s.charAt(i - dp[i] - 1) == s.charAt(i + dp[i] + 1)) {
-                dp[i]++;
-            }
-
-            if (i + dp[i] > right) {
-                center = i;
-                right = i + dp[i];
-            }
-
-            if (dp[i] > maxLen) {
-                maxLen = dp[i];
-                maxStr = s.substring(i - dp[i], i + dp[i] + 1).replaceAll("#", "");
+        for(int i = s.length(); i > 1; i--) {
+            for(int j = 0; j + i <= s.length(); j++) {
+                String str = s.substring(j, j + i);
+                boolean isValid = true;
+                for(int k = 0; k < str.length() / 2; k++) {
+                    if(str.charAt(k) != str.charAt(str.length() - k - 1)) {
+                        isValid = false;
+                        break;
+                    }
+                }
+                if(isValid) {
+                    return str;
+                }
             }
         }
-
-        return maxStr;
+        return s.substring(0,1);
     }
 }
