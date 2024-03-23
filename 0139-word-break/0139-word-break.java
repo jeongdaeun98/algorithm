466d0b63d9bc1338f125745aebc3cf88d8fb4465
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-03-23 11:09:49 +0900

    Time: 4 ms (72.61%), Space: 44 MB (56.20%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,19 +1,21 @@
+// s 배열 false로 초기화
+// wordDict를 hashmap으로 초기화
+// 순회
+// hashMap에 있다면 그 다음 index true로
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> dicts = new HashSet<>();
-        for(String word : wordDict) {
-            dicts.add(word);
-        }
-        boolean dp[] = new boolean[s.length() + 1];
-        dp[0] = true;
+        boolean[] contains = new boolean[s.length() + 1];
+        Set<String> words = new HashSet<>(wordDict);
+        contains[0] = true;
         for(int i = 1; i <= s.length(); i++) {
             for(int j = i - 1; j >= 0; j--) {
-                if(dp[j] && dicts.contains(s.substring(j, i))) {
-                    dp[i] = true;
+                String str = s.substring(j, i);
+                if(contains[j] && words.contains(str)) {
+                    contains[i] = true;
                     break;
                 }
             }
         }
-        return dp[s.length()];
+        return contains[s.length()];
     }
 }
