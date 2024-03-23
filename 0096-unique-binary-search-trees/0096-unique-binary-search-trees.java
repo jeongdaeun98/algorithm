e1cf346e7970e6bfcc821e6401b372ac5f2c8d95
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-03-23 11:36:12 +0900

    Time: 0 ms (100.00%), Space: 40.7 MB (9.99%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,14 +1,25 @@
+// 1 1
+// 0 * 1 = 1
+// 2 2
+// 0 * 1 = 1
+// 1 * 0 = 1
+// 3 5
+// 0 * 2 = 2
+// 2 * 0 = 2
+// 1 * 1 = 1
+// 4 14
+// 0 * 3 = 5
+// 3 * 0 = 5
+// 1 * 2 = 2
+// 2 * 1 = 2
+// 5 
 class Solution {
     public int numTrees(int n) {
         int dp[] = new int[n + 1];
         dp[0] = 1;
-        dp[1] = 1;
-        if(n >= 2) {
-            dp[2] = 2;
-        }
-        for(int i = 3; i <= n; i++) {
-            for(int j = 0; j < i; j++) {
-                dp[i] += dp[j] * dp[i - j - 1];
+        for(int i = 1; i <= n; i++) {
+            for(int j = i - 1; j >= 0; j--){
+                dp[i] += dp[j] *dp[i - j - 1];
             }
         }
         return dp[n];
