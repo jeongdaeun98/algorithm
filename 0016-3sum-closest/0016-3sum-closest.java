fe82733e145ab6794512d699382c8dc4faba26c4
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-03-24 23:41:23 +0900

    Time: 14 ms (72.24%), Space: 42.9 MB (79.32%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,18 +1,21 @@
+// 순회하면서 left와 right의 sum을 저장
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
+        int absMinusNum = Integer.MAX_VALUE, answer = nums[0] + nums[1] + nums[2];
         Arrays.sort(nums);
-        int answer = nums[0] + nums[1] + nums[2];
         for(int i = 0; i < nums.length; i++) {
             int left = i + 1, right = nums.length - 1;
             while(left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-                if(Math.abs(target - sum) < Math.abs(target - answer)) {
+                int sum = nums[left] + nums[right] + nums[i];
+                if(absMinusNum > Math.abs(target - sum)) {
                     answer = sum;
+                    absMinusNum = Math.abs(target - sum);
                 }
                 if(target > sum) {
                     left++;
                 } else {
-                    right--;
+                    right --;
+                    
                 }
             }
         }
