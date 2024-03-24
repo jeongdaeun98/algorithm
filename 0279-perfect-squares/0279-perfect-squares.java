253ed4cf42ba05150ac33a5c83709928ef6109b7
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-03-25 00:10:16 +0900

    Time: 37 ms (38.69%), Space: 42.3 MB (86.76%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,21 +1,13 @@
-// 1 1
-// 2 1 + 1 = 2
-// 3 2 + 1 = 3
-// 4 4 = 1
-// 5 4 1 = 2
-// 6 4 2 = 3
-// 7 4 3 = 4
-// 8 4 4 = 2
-// 9 4 5 = 2 + 1 = 3
-// 12 
 class Solution {
     public int numSquares(int n) {
         int dp[] = new int[n + 1];
         Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = 0;
+        dp[1] = 1;
+        dp[0] = 1;
         for(int i = 1; i <= n; i++) {
             for(int j = 1; j * j <= i; j++) {
-                dp[i] = Math.min(dp[i], dp[i - j*j] + 1);
+                dp[j * j] = 1;
+                dp[i] = Math.min(dp[i - j * j] + 1, dp[i]);
             }
         }
         return dp[n];
