f514dcba5d36eedd444b6e583d939e4a12882586
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-03-24 21:42:02 +0900

    Time: 32 ms (53.93%), Space: 42.9 MB (30.59%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,14 +1,21 @@
-// 1부터 n까지 순회
-// 1*1 2*2 3*3 ... n*n  = 1
-// 순회하면서 dp[n - j * j] + 1
+// 1 1
+// 2 1 + 1 = 2
+// 3 2 + 1 = 3
+// 4 4 = 1
+// 5 4 1 = 2
+// 6 4 2 = 3
+// 7 4 3 = 4
+// 8 4 4 = 2
+// 9 4 5 = 2 + 1 = 3
+// 12 
 class Solution {
     public int numSquares(int n) {
-        int[] dp = new int[n + 1];
+        int dp[] = new int[n + 1];
         Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
         for(int i = 1; i <= n; i++) {
             for(int j = 1; j * j <= i; j++) {
-                dp[i] = Math.min(dp[i - j * j] + 1, dp[i]);
+                dp[i] = Math.min(dp[i], dp[i - j*j] + 1);
             }
         }
         return dp[n];
