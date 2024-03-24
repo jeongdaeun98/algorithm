886ef5bfd85b4e29287e95ae319f8def97f84ba6
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-03-25 00:01:26 +0900

    Time: 273 ms (5.74%), Space: 44.6 MB (69.00%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,16 +1,15 @@
-// 순회
-// 다음 수부터 순회하면서 곱한 값 중 가장 큰 값으로 갱신
+// 
 class Solution {
     public int maxProduct(int[] nums) {
-        int maxNum = Integer.MIN_VALUE;
+        int answer = Integer.MIN_VALUE;
         for(int i = 0; i < nums.length; i++) {
             int mul = nums[i];
             for(int j = i + 1; j < nums.length; j++) {
-                maxNum = Math.max(maxNum, mul);
+                answer = Math.max(answer, mul);
                 mul *= nums[j];
             }
-            maxNum = Math.max(maxNum, mul);
+            answer = Math.max(answer, mul);
         }
-        return maxNum;
+        return answer;
     }
 }
