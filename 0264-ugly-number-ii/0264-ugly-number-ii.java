f8eb8dc8ba13a1d48135788ec9267d205ad2454d
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-03-24 21:24:09 +0900

    Time: 2 ms (98.40%), Space: 41.8 MB (44.60%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.java
+++ 0264-ugly-number-ii/0264-ugly-number-ii.java
@@ -1,16 +1,26 @@
-//1. 2, 3, 5의 배수 중 가장 작은 값을 ugly 수라고 설정한다.
-//2. 설정된 값에서 곱한 값을 증가시키면서 ugly 넘버를 구한다. 
+// 2, 3, 5에 곱해지는 수를 1부터 count가 n이 될 때까지 더함
+// 가장 작은 수를 ugly number로 설정
 class Solution {
     public int nthUglyNumber(int n) {
-        int ugly[] = new int[n + 1];
-        int two = 1, three = 1, five = 1;
-        ugly[1] = 1;
-        for(int i = 2; i <= n; i++) {
-            ugly[i] = Math.min(Math.min(ugly[two]*2, ugly[three]*3), ugly[five]*5);
-            if(ugly[i] == ugly[two] * 2) two++;
-            if(ugly[i] == ugly[three] * 3) three++;
-            if(ugly[i] == ugly[five] * 5) five++;
+        int twoMultiply = 0, threeMultiply = 0, fiveMultiply = 0;
+        int ugly[] = new int[n];
+        int index = 1;
+        ugly[0] = 1;
+        while(n != index) {
+            int uglyNumber = Math.min(ugly[twoMultiply] * 2, Math.min(ugly[threeMultiply] * 3, ugly[fiveMultiply] * 5));
+                ugly[index] = uglyNumber;
+            
+            if(uglyNumber % 2 == 0) {
+                twoMultiply++;
+            }
+            if(uglyNumber % 3 == 0) {
+                threeMultiply++;
+            }
+            if(uglyNumber % 5 == 0) {
+                fiveMultiply++;
+            }
+            index++;
         }
-        return ugly[n];
+        return ugly[n - 1];
     }
 }
