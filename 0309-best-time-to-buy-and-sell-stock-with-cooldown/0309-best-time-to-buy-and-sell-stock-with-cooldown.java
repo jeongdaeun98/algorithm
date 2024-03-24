e600ae5e0b5d52d3cac2ac06f6ea629593683076
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-03-24 22:17:28 +0900

    Time: 1 ms (80.37%), Space: 41.7 MB (41.65%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,16 +1,15 @@
-// [1,2,3,0,2]
-// buy[i] = Math.max(rest[i - 1] - prices, buy[i - 1])
+// buy[i] = Math.max(rest[i - 1] - prices[i], buy[i - 1])
+// rest[i] = Math.max(buy[i - 1], rest[i - 1], sell[i - 1])
 // sell[i] = Math.max(buy[i - 1] + prices[i], sell[i - 1])
-// rest[i] = Math.max(sell[i - 1], buy[i - 1], rest[i - 1])
-// buy[i] + sell[i] + rest[i]
-// buy = Math.max(sell[i - 2] - prices[i] , buy[i - 1])
-// sell = Math.max(buy[i - 1] + prices[i], sell[i - 1])
-// Math.max(buy[i - 2] - prices[i] , buy[i - 1]) + Math.max(buy[i - 1] + prices[i], sell[i - 1]) + buy[i - 1]
-
+// buy[i] = Math.max(sell[i - 2] - prices[i], buy[i - 1])
+// prevBuy = buy0
+// buy = Math.max(prevSell - prices[i], buy)1
+// prevSell = sell 
+// sell = Math.max(prevBuy + prices[i], sell)
 class Solution {
     public int maxProfit(int[] prices) {
-        int prevBuy = Integer.MIN_VALUE, buy = Integer.MIN_VALUE, sell = 0, prevSell = 0;
-        for(int i = 0; i < prices.length; i++){
+        int buy = Integer.MIN_VALUE, sell= 0, prevSell = 0, prevBuy = Integer.MIN_VALUE;
+        for(int i = 0; i < prices.length; i++) {
             prevBuy = buy;
             buy = Math.max(prevSell - prices[i], buy);
             prevSell = sell;
