4a6f234b10f99edc894a31ccc784f143dddae6c3
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-03-24 17:48:49 +0900

    Time: 269 ms (10.46%), Space: 44.6 MB (80.89%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,17 +1,16 @@
-// 순회하면서
-// 순회하는 인덱스보다 큰 인덱스들을 순회하면서 계속 곱해가면서 가장 큰 값을 저장
-// 
+// 순회
+// 다음 수부터 순회하면서 곱한 값 중 가장 큰 값으로 갱신
 class Solution {
     public int maxProduct(int[] nums) {
-        int result = nums[0];
+        int maxNum = Integer.MIN_VALUE;
         for(int i = 0; i < nums.length; i++) {
             int mul = nums[i];
             for(int j = i + 1; j < nums.length; j++) {
-                result = Math.max(result, mul);
+                maxNum = Math.max(maxNum, mul);
                 mul *= nums[j];
             }
-            result = Math.max(result, mul);
+            maxNum = Math.max(maxNum, mul);
         }
-        return result;
+        return maxNum;
     }
 }
