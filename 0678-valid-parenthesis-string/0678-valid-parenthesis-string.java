95421defc0c7d78d60e085deb6a06ff0b823b62e
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-03-25 10:33:13 +0900

    Time: 0 ms (100.00%), Space: 41.7 MB (14.62%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,15 +1,18 @@
+// leftMax leftMin으로 계산
+// max일 경우 *에 대해 ++, min은 *에 대해 --
+// min이 0보다 클 경우 
 class Solution {
     public boolean checkValidString(String s) {
         int leftMax = 0, leftMin = 0;
         for(int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) == '(') {
-               leftMax++;
-                leftMin++;
-            } else if(s.charAt(i) == ')') {
+            if(s.charAt(i) == ')'){
                 leftMax--;
                 if(leftMin > 0) {
                     leftMin--;
                 }
+            } else if(s.charAt(i) == '(') {
+                leftMax++;
+                leftMin++;
             } else {
                 leftMax++;
                 if(leftMin > 0) {
