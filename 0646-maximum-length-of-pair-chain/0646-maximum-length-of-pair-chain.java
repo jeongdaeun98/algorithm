f38ebdc46d68245411fe2f679d180955b21da281
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-03-25 14:59:58 +0900

    Time: 9 ms (74.59%), Space: 44.7 MB (89.34%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
@@ -1,14 +1,13 @@
-// 다음 인자의 left는 반드시 인자의 right보다 크기 때문에
-// 정렬
+// 소팅
 // 순회
-// right를 마지막 range로 설정
-// 다음 인자의 left와 비교하여 left가 클 경우, answer증가하면서 마지막 range 를 right로 설정
+// range의 마지막 범위 저장
+// 마지막 범위 < 첫번째 범위라면, answer 증가 마지막 범위 갱신
 class Solution {
     public int findLongestChain(int[][] pairs) {
-        Arrays.sort(pairs, (a,b) -> a[1] - b[1]);
+        Arrays.sort(pairs,(a, b) -> a[1] - b[1]);
         int answer = 1;
         int lastRange = pairs[0][1];
-        for(int i = 1; i < pairs.length; i++) {
+        for(int i = 0; i < pairs.length; i++) {
             if(lastRange < pairs[i][0]) {
                 answer++;
                 lastRange = pairs[i][1];
