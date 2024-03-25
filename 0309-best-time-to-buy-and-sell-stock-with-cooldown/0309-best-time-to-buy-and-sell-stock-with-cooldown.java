2e3e82200da2c1ce7318df0472b8b4ee2fb0a6fa
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-03-25 10:47:43 +0900

    Time: 1 ms (80.39%), Space: 41.6 MB (64.39%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,15 +1,11 @@
 // buy[i] = Math.max(rest[i - 1] - prices[i], buy[i - 1])
-// rest[i] = Math.max(buy[i - 1], rest[i - 1], sell[i - 1])
-// sell[i] = Math.max(buy[i - 1] + prices[i], sell[i - 1])
+// rest[i] = Math.max(buy[i - 1], sell[i -1], rest[i -1])
 // buy[i] = Math.max(sell[i - 2] - prices[i], buy[i - 1])
-// prevBuy = buy0
-// buy = Math.max(prevSell - prices[i], buy)1
-// prevSell = sell 
-// sell = Math.max(prevBuy + prices[i], sell)
+// sell[i] = Math.max(buy[i - 1] + prices[i], sell[i - 1])
 class Solution {
     public int maxProfit(int[] prices) {
-        int buy = Integer.MIN_VALUE, sell= 0, prevSell = 0, prevBuy = Integer.MIN_VALUE;
-        for(int i = 0; i < prices.length; i++) {
+        int prevBuy = Integer.MIN_VALUE, buy = Integer.MIN_VALUE, sell = 0, prevSell = 0;
+        for(int i = 0 ; i < prices.length; i++) {
             prevBuy = buy;
             buy = Math.max(prevSell - prices[i], buy);
             prevSell = sell;
