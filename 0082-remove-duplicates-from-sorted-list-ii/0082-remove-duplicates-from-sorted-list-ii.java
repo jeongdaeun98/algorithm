d3bee144f967102b3bd1d969a86357560af59f79
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-03-26 23:58:48 +0900

    Time: 1 ms (18.95%), Space: 43 MB (56.11%) - LeetHub

---
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+// 이전 노드와 중복될 경우 ListNode 갱신
+public class Solution {
+public ListNode deleteDuplicates(ListNode head) {
+	//use two pointers, slow - track the node before the dup nodes, 
+	// fast - to find the last node of dups.
+    ListNode dummy = new ListNode(0), fast = head, slow = dummy;
+    slow.next = fast;
+    while(fast != null) {
+    	while (fast.next != null && fast.val == fast.next.val) {
+     		fast = fast.next;    //while loop to find the last node of the dups.
+    	}
+    	if (slow.next != fast) { //duplicates detected.
+    		slow.next = fast.next; //remove the dups.
+    		fast = slow.next;     //reposition the fast pointer.
+    	} else { //no dup, move down both pointer.
+    		slow = slow.next;
+    		fast = fast.next;
+    	}
+    	
+    }
+    return dummy.next;
+} }
