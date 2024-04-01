337e8203ebb827b5ea1b294eb83872b0e8d411b3
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-04-01 23:16:32 +0900

    Time: 0 ms (100.00%), Space: 41.6 MB (81.58%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,14 +1,18 @@
+// left = 0, right = nums.length - 1
+// i로 순회
+// i가 0일 경우, left와 i swap 후 left와 i 증가
+// i가 1일 경우, i 증가
+// i가 2일 경우, right와 i swap 후 r만 감소
 class Solution {
     public void sortColors(int[] nums) {
-        int l = 0;
-        int r = nums.length - 1;
-        for(int i = 0; i <= r;) {
+        int left = 0, right = nums.length - 1;
+        for(int i = 0; i <= right;) {
             if(nums[i] == 0) {
-                swap(nums, i++, l++);
-            } else if(nums[i] == 1) {
-                ++i;
-            } else {
-                swap(nums, i, r--);
+                swap(nums, i++, left++);
+            }else if(nums[i] == 1) {
+                i++;
+            }else {
+                swap(nums, i, right--);
             }
         }
     }
